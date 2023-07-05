e64b1973a43e9d4041bd4141fd5d734f7a891f8e
Nykuluz <carlmillanes3@gmail.com>
2023-07-05 11:26:28 +0800

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Carl Irven Nykuluz F. Millanes on 7/5/23.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
